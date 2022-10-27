3f27a4d1ade1e250c308d40de2d000bd25b415db
Dios-Fer <fernandoort.alumn@cescristorey.com>
2022-10-27 12:06:31 +0200

    pre rama

--- suma2.cpp
+++ suma2.cpp
@@ -15,7 +15,7 @@ int main() {
 /*las dos anteriores hacen lo mismo que con el primer digito (a), pero con el segundo digito (b)*/
 	res = a + b;
 /*Aqui le indicamos a la unidad de control que tome el valor numerico que ocupa la posicion de memoria "a" de la memoria principal, lo traiga a la ALU y esta opere, la operacion se le indicara, en nuestro caso en nuestro lenguaje de alto nivel lo indicamos como "+", lo mismo que paso para traer a "a" lo hara con "b". Y tambien le indicamos que tras operar guarde el resultado en un espacio de la memoria principal (variable), el cual es "res"*/
-	cout << "El resultado de tu suma es: "<< res << endl;
+	cout << "El resultado de tu suma es: siuuu"<< res << endl;
 /*Traemos a la pantalla el texto entre comillas, ademas de lo que ocupa la variable "res" y por ultimo un salto de linea*/
 }
 /*nos indica que nuestro programa ha finalizado con exito y tras esto limpia todos los bits empleados*/
